cc2ad0fcbd6f81c742c1f8e8a9a0a0f49ba1ec98
brenko <514386@muni.cz>
2022-07-07 12:14:33 +0200

    feat: PerunEnsureMember

---
+++ microservices/perun_ensure_member.py
@@ -0,0 +1,200 @@
+from perun.connector.utils.Logger import Logger
+from perun.connector.models.MemberStatusEnum import MemberStatusEnum
+from perun.connector.adapters.AdaptersManager import AdaptersManager
+from satosa.micro_services.base import ResponseMicroService
+from satosa.exception import SATOSAError
+from satosa.response import Redirect
+from utils.ConfigStore import ConfigStore
+from utils.PerunConstants import PerunConstants
+
+class PerunEnsureMember(ResponseMicroService):
+    def __init__(self, config, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.LOG_PREFIX = 'perun:PerunEnsureMember: '
+        self.REGISTER_URL = 'registerUrl'
+        self.VO_SHORT_NAME = 'voShortName'
+        self.GROUP_NAME = 'groupName'
+        self.CALLBACK_PARAMETER_NAME = 'callbackParameterName'
+        self.CALLBACK = '' # TODO
+        self.PARAM_REGISTRATION_URL = 'registrationUrl'
+        self.TEMPLATE = '' # TODO
+
+        self.__logger = Logger.get_logger(self.__class__.__name__)
+
+        self.__config = config
+        self.__global_cfg = ConfigStore.get_global_cfg(
+            config["global_cfg_path"]
+        )
+        self.__attr_map_cfg = ConfigStore.get_attributes_map(
+            self.__global_cfg["attr_cfg_path"]
+        )
+        self.__adapters_manager = AdaptersManager(
+            self.__global_cfg,
+            self.__attr_map_cfg
+        )
+
+        if self.REGISTER_URL not in self.__config \
+                or not self.__config[self.REGISTER_URL]:
+            raise SATOSAError(
+                self.LOG_PREFIX + 'Missing configuration option \''
+                + self.REGISTER_URL + '\''
+            )
+        self.__register_url = self.__config[self.REGISTER_URL]
+
+        if self.CALLBACK_PARAMETER_NAME not in self.__config \
+                or not self.__config[self.CALLBACK_PARAMETER_NAME]:
+            raise SATOSAError(
+                self.LOG_PREFIX + 'Missing configuration option \''
+                + self.CALLBACK_PARAMETER_NAME + '\''
+            )
+        self.__callback_param_name = self.__config[self.CALLBACK_PARAMETER_NAME] # noqa e501
+
+        if self.VO_SHORT_NAME not in self.__config \
+                or not self.__config[self.VO_SHORT_NAME]:
+            raise SATOSAError(
+                self.LOG_PREFIX + 'Missing configuration option \''
+                + self.VO_SHORT_NAME + '\''
+            )
+        self.__vo_short_name = self.__config[self.VO_SHORT_NAME]
+
+        self.__group_name = self.__config[self.GROUP_NAME]
+
+        self.__unauthorized_redirect_url = \
+            self.__config["unauthorized_redirect_url"]
+
+    def process(self, context, data):
+        if data.data['perun']['user']:
+            user = data.data['perun']['user']
+        else:
+            raise SATOSAError(
+                self.LOG_PREFIX + 'Missing mandatory field '
+                '\'perun.user\' in request. Hint: Did you '
+                'configured PerunIdentity filter before this filter?'
+            )
+        vo = self.__adapters_manager.get_vo(short_name=self.__vo_short_name)
+        if not vo:
+            raise SATOSAError(
+                self.LOG_PREFIX + 'VO with vo_short_name \''
+                + self.__vo_short_name + '\' not found.'
+            )
+
+        self.__handle_user(user, vo, data, context)
+
+        return super().process(context, data)
+
+    def __handle_user(self, user, vo, data, context):
+        is_user_in_group = not self.__group_name or self.__is_user_in_group(user, vo) # noqa e501
+        member_status = self.__adapters_manager.get_member_status_by_user_and_vo(user, vo) # noqa e501
+
+        if member_status == MemberStatusEnum.VALID and is_user_in_group:
+            self.__logger.debug(
+                self.LOG_PREFIX + 'User is allowed to continue.'
+            )
+
+            return
+
+        member_status = self.__adapters_manager.get_member_status_by_user_and_vo(user, vo)  # noqa e501
+        # TODO implement has_registration_form()
+        vo_has_registration_form = self.__adapters_manager.has_registration_form(vo.id, PerunConstants.VO) # noqa e501
+        group_has_registration_form = self.__group_has_registration_form(vo) # noqa e501
+
+        if member_status == MemberStatusEnum.VALID and is_user_in_group:
+            self.__logger.debug(
+                self.LOG_PREFIX + 'User is allowed to continue.'
+            )
+        elif member_status == MemberStatusEnum.VALID \
+                and not is_user_in_group and group_has_registration_form:
+            self.__logger.debug(
+                self.LOG_PREFIX + 'User is not valid in group ' +
+                self.__group_name + ' - sending to registration.'
+            )
+            self.__register(context, data)
+        elif not member_status and vo_has_registration_form and is_user_in_group: # noqa e501
+            self.__logger.debug(
+                self.LOG_PREFIX + 'User is not member of vo ' +
+                self.__vo_short_name + ' - sending to registration.'
+            )
+            self.__register(context, data)
+        elif not member_status and vo_has_registration_form and \
+                not is_user_in_group and group_has_registration_form:
+            self.__logger.debug(
+                self.LOG_PREFIX + 'User is anot valid in group ' +
+                self.__group_name + ' - sending to registration.'
+            )
+            self.__register(context, data, self.__group_name)
+        elif member_status == MemberStatusEnum.EXPIRED \
+                and vo_has_registration_form and is_user_in_group:
+            self.__logger.debug(
+                self.LOG_PREFIX + 'User is expired - sending to registration.'
+            )
+            self.__register(context, data)
+        elif member_status == MemberStatusEnum.EXPIRED \
+                and vo_has_registration_form and not is_user_in_group \
+                and group_has_registration_form:
+            self.__logger.debug(
+                self.LOG_PREFIX + 'User is expired and not in group '
+                + self.__group_name + ' - sending to registration.'
+            )
+            self.__register(context, data, self.__group_name)
+        else:
+            self.__logger.debug(
+                self.LOG_PREFIX + 'User is  not valid in vo/group and cannot'
+                ' be sent to the registration - sending to unauthorized'
+            )
+            self.__unauthorized(context, data)
+
+
+    def __is_user_in_group(self, user, vo):
+        member_groups = self.__adapters_manager.get_member_groups(user, vo)
+
+        for group in member_groups:
+            if self.__group_name == group.name:
+                return True
+
+        return False
+
+    def __group_has_registration_form(self, vo):
+        try:
+            group = self.__adapters_manager.get_group_by_name(vo, self.__group_name) # noqa e501
+        except Exception as e:
+            group = None
+
+        if group is not None:
+            return self.__adapters_manager.has_registration_form(group.id, PerunConstants.GROUP) # noqa e501
+
+        return False
+
+    def __register(self, context, data, group_name=None):
+        params = {}
+
+        callback = '' # TODO (any hint?)
+
+        if self.__callback_param_name:
+            registration_url = self.__register_url + '?vo=' \
+                               + self.__vo_short_name
+            if group_name:
+                registration_url += '&group=' + group_name
+
+            params[PerunConstants.TARGET_NEW] = callback
+            params[PerunConstants.TARGET_EXISTING] = callback
+            params[PerunConstants.TARGET_EXTENDED] = callback
+
+            self.__logger.debug(
+                self.LOG_PREFIX + 'Redirecting to \'' + registration_url
+                + ', callback parameter \'' + self.__callback_param_name
+                + '\' set to value \'' + callback + '\'.'
+            )
+            data.attributes = params
+            context.state[self.name] = data.to_dict()
+
+            Redirect(registration_url)
+
+        else:
+            raise SATOSAError(
+                self.LOG_PREFIX +  'No configuration for registration set. Cannot proceed.' # noqa e501
+            )
+
+    def __unauthorized(self, context, data):
+        context.state[self.name] = data.to_dict()
+        return Redirect(self.__unauthorized_redirect_url)
