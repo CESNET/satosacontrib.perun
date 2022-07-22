605612a458faf91fca70c6f81c9145e130a8ae37
Peter Bolha <xbolha@fi.muni.cz>
2022-07-22 11:58:59 +0200

    feat: sp_authorization_microservice

---
+++ satosacontrib/perun/utils/PerunConstants.py
@@ -0,0 +1,16 @@
+class PerunConstants:
+    TARGET_NEW = 'targetnew'
+
+    TARGET_EXISTING = 'targetexisting'
+
+    TARGET_EXTENDED = 'targetextended'
+
+    PERUN = 'perun'
+
+    USER = 'user'
+
+    VO = 'vo'
+
+    GROUP = 'group'
+
+    GROUP_MEMBERS = 'members'
